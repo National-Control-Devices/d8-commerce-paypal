062ab1662d3da8c9209822e10386111e3fbd2c5e
Trey Felton <trey@Your-Daddy.local>
2015-09-29 09:46:01 -0500

    Initial Commit

---
+++ paypal/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,22 @@
+<?php
+namespace Drupal\paypal\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ * )
+ */
+class HelloBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->t('Hello, World!'),
+    );
+  }
+}
+?>
